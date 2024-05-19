cff4ba5de33531d1430ad7d434e7e4d0d3292945
MrFrangipane <contact@frangipane.me>
2024-05-19 21:06:14 +0200

    try to debounce OSC: try to fix MIDI lags on Pi

--- xtouchqusb/__main__.py
+++ xtouchqusb/__main__.py
@@ -1,12 +1,16 @@
+import time
+
+from mido.messages import Message
+from mido.sockets import connect, SocketPort
+from mido.ports import BaseInput, BaseOutput
+
 import logging
 import json
 import os.path
 import sys
 
-import mido
-
-from xtouchqusb.components.application import Application
-
+from xtouchqusb.python_extensions.mido_extensions import open_input_from_pattern, open_output_from_pattern
+from xtouchqusb.components.qu_sb import QuSb
 
 _logger = logging.getLogger(__name__)
 
@@ -14,10 +18,31 @@ _logger = logging.getLogger(__name__)
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
 
-    if len(sys.argv) == 2:
-        configuration_filepath = os.path.join(os.path.dirname(__file__), 'resources', sys.argv[1])
-        application = Application()
-        application.load_configuration(configuration_filepath)
-        application.exec()
-    else:
-        raise AttributeError('No configuration filepath provided')
+    midi_tcp = connect('192.168.20.4', 51325)
+    midi_in = open_input_from_pattern('QU-SB')
+    midi_out = open_output_from_pattern('QU-SB')
+
+    try:
+        messages = list()
+        request_message = Message(
+            type='sysex',
+            data=QuSb.SYSEX_HEADER + QuSb.SYSEX_ALL_CALL + QuSb.SYSEX_GET_SYSTEM_STATE + b'\x00'  # we are not an iPad
+        )
+        begin = time.time()
+        midi_tcp.send(request_message)
+        for message in midi_tcp:
+            if bytearray(message.bytes()[1:-1]) == QuSb.SYSEX_HEADER + b'\x00' + QuSb.SYSEX_SYSTEM_STATE_END:
+                midi_tcp.close()
+                break
+            messages.append((time.time(), message))
+
+        print(f'Elapsed: {messages[-1][0] - begin}')
+        print(f'Number of messages: {len(messages)}')
+
+    except KeyboardInterrupt:
+        pass
+
+    finally:
+        midi_tcp.close()
+        midi_in.close()
+        midi_out.close()
