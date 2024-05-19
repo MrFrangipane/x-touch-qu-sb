ef85db67d223a2926628f6d7ed6f057b34762fd3
MrFrangipane <contact@frangipane.me>
2024-05-19 21:17:46 +0200

    try to debounce OSC: try to fix MIDI lags on Pi

--- xtouchqusb/__main__.py
+++ xtouchqusb/__main__.py
@@ -9,31 +9,37 @@ import json
 import os.path
 import sys
 
+from multiprocessing import Process
+
 from xtouchqusb.python_extensions.mido_extensions import open_input_from_pattern, open_output_from_pattern
 from xtouchqusb.components.qu_sb import QuSb
 
 _logger = logging.getLogger(__name__)
 
 
+def request_state():
+    request_message = Message(
+        type='sysex',
+        data=QuSb.SYSEX_HEADER + QuSb.SYSEX_ALL_CALL + QuSb.SYSEX_GET_SYSTEM_STATE + b'\x00'  # we are not an iPad
+    )
+    with connect('192.168.20.4', 51325) as midi_tcp:
+        midi_tcp.send(request_message)
+
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
 
-    midi_tcp = connect('192.168.20.4', 51325)
     midi_in = open_input_from_pattern('QU-SB')
-    # midi_out = open_output_from_pattern('QU-SB')
+
+    begin = time.time()
+    p = Process(target=request_state)
+    p.start()
 
     try:
         messages = list()
-        request_message = Message(
-            type='sysex',
-            data=QuSb.SYSEX_HEADER + QuSb.SYSEX_ALL_CALL + QuSb.SYSEX_GET_SYSTEM_STATE + b'\x00'  # we are not an iPad
-        )
-        begin = time.time()
-        midi_tcp.send(request_message)
         for message in midi_in:
             messages.append((time.time(), message))
             if bytearray(message.bytes()[1:-1]) == QuSb.SYSEX_HEADER + b'\x00' + QuSb.SYSEX_SYSTEM_STATE_END:
-                midi_tcp.close()
                 break
 
         print(f'Elapsed: {messages[-1][0] - begin}')
@@ -43,6 +49,5 @@ if __name__ == '__main__':
         pass
 
     finally:
-        # midi_tcp.close()
         midi_in.close()
         # midi_out.close()
