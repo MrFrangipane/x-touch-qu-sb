00a758be9f0f27c9a10f9baf1bb516cfea4bb983
MrFrangipane <contact@frangipane.me>
2024-05-20 10:32:46 +0200

    try to debounce OSC: try to fix MIDI lags on Pi

--- xtouchqusb/__main__.py
+++ xtouchqusb/__main__.py
@@ -17,13 +17,29 @@ from xtouchqusb.components.qu_sb import QuSb
 _logger = logging.getLogger(__name__)
 
 
-def request_state():
+def request_state() -> list[Message]:
+    _logger.info(f"Requesting Qu-SB state...")
+    _begin = time.time()
+    messages: list[Message] = list()
+
     request_message = Message(
         type='sysex',
         data=QuSb.SYSEX_HEADER + QuSb.SYSEX_ALL_CALL + QuSb.SYSEX_GET_SYSTEM_STATE + b'\x00'  # we are not an iPad
     )
     with connect('192.168.20.4', 51325) as midi_tcp:
         midi_tcp.send(request_message)
+
+        while True:
+            message = midi_tcp.receive()
+            if bytearray(message.bytes()[1:-1]) == QuSb.SYSEX_HEADER + b'\x00' + QuSb.SYSEX_SYSTEM_STATE_END:
+                break
+            messages.append(message)
+
+        midi_tcp.close()
+
+    _logger.info(f"Done in {time() - begin:.3f}s")
+
+    return messages
 
 
 if __name__ == '__main__':
@@ -34,22 +50,14 @@ if __name__ == '__main__':
     p.start()
 
     try:
-        messages = list()
+        state_messages = request_state()
+
         port_name = get_input_port_name_from_pattern('QU-SB')
         midi_in, port_name = open_midiinput(port_name)
 
-        with open("out.txt", "w+") as txt:
-
-            while True:
-                message = midi_in.get_message()
-                messages.append((time.time(), message))
-                # if bytearray(message.bytes()[1:-1]) == QuSb.SYSEX_HEADER + b'\x00' + QuSb.SYSEX_SYSTEM_STATE_END:
-                #     break
-                # print(message)
-                txt.write(str(message) + '\n')
-
-        print(f'Elapsed: {messages[-1][0] - begin}')
-        print(f'Number of messages: {len(messages)}')
+        while True:
+            message = midi_in.get_message()
+            print(message)
 
     except KeyboardInterrupt:
         pass
